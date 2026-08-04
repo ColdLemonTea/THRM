efec1731f9a091770b4b2e5e841fae515968e5a4
Forst <ForstLimonCat@pm.me>
2026-08-04 12:09:02 +0800

    fix(linux): use native Plasma window decorations

--- ui/linux/runner/my_application.cc
+++ ui/linux/runner/my_application.cc
@@ -30,9 +30,9 @@ static void my_application_activate(GApplication* application) {
   // desktop).
   // If running on X and not using GNOME then just use a traditional title bar
   // in case the window manager does more exotic layout, e.g. tiling.
-  // If running on Wayland assume the header bar will work (may need changing
-  // if future cases occur).
-  gboolean use_header_bar = TRUE;
+  // On Wayland, let KWin provide Plasma's native window decorations.
+  gboolean use_header_bar =
+      g_strcmp0(g_getenv("XDG_CURRENT_DESKTOP"), "KDE") != 0;
 #ifdef GDK_WINDOWING_X11
   GdkScreen* screen = gtk_window_get_screen(window);
   if (GDK_IS_X11_SCREEN(screen)) {
